ceed64ef5f8d122a5efd1bb0964da01cb26f31b2
SomeoneDifferent <45271579+SomeoneDifferent@users.noreply.github.com>
2018-11-22 22:22:27 +0300

    Add files via upload

---
+++ UserApi/UserApi/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using UserApi.Models;
+
+namespace UserApi.Controllers
+{
+    [Route("api/{MethodName}")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserContext _userContext;
+
+        public UserController(UserContext userContext) => _userContext = userContext;
+
+        [HttpGet("{Id}", Name = "User")]
+        public IActionResult Get(int id)
+        {
+            User user = _userContext.Users.Find(id);
+
+            if (user == null)
+                return NotFound(); 
+
+            return Ok(user);
+        }
+        
+        [HttpPost]
+        public IActionResult Create([FromBody] User user)
+        {
+            if (user == null) 
+                return BadRequest();
+
+            User findingUser = _userContext.Users.Find(user.Id);
+
+            if (findingUser != null)
+                return BadRequest();
+
+            _userContext.Users.Add(user);
+            _userContext.SaveChanges();
+
+            return CreatedAtRoute("User", new { id = user.Id }, user);
+        }
+    }
+}
---
+++ UserApi/UserApi/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using UserApi.Models;
+
+namespace UserApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase("Users"));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app) => app.UseMvc();
+    }
+}
